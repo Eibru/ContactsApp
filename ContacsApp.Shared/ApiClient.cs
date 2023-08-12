1c22fcfae5203a9adb8fb01c46c52e19877ea408
EiBru <eilev.brustugun@outlook.com>
2023-08-12 12:00:46 +0200

    Added IApiClient interface to ApiClient.cs file

--- ContacsApp.Shared/ApiClient.cs
+++ ContacsApp.Shared/ApiClient.cs
@@ -4,6 +4,14 @@ using ContactsApp.Shared.Models;
 
 namespace ContactsApp.Shared;
 
+public interface IApiClient {
+    public Task<Contact> GetContact(int id);
+    public Task<List<Contact>> GetContacts();
+    public Task UpdateContact(Contact contact);
+    public Task<Contact> CreateContact(Contact contact);
+    public Task DeleteContact(int id);
+}
+
 public class ApiClient : IApiClient{
     private readonly HttpClient _httpClient;
 
